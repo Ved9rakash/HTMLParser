6a109f15a072630ab97af79a036bd7ba82a4d116
Ved9rakash <vs19990711@gmail.com>
2021-09-15 11:37:37 +0530

    initial commit

---
+++ cmake-build-debug/CMakeFiles/HTMLParser.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HTMLParser.dir/main.cpp.obj"
+  "HTMLParser.exe"
+  "HTMLParser.exe.manifest"
+  "HTMLParser.pdb"
+  "libHTMLParser.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HTMLParser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
