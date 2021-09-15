2d239081706290b644f94d106368c6cf19063817
Ved9rakash <vs19990711@gmail.com>
2021-09-15 16:29:06 +0530

    Parsing file

---
+++ include/convert.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Vaibhav on 15-09-2021.
+//
+
+#ifndef HTMLPARSER_CONVERT_HPP
+#define HTMLPARSER_CONVERT_HPP
+
+class Convert
+{
+    explicit Convert() = default;
+
+};
+
+#endif //HTMLPARSER_CONVERT_HPP
---
+++ src/convert.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Vaibhav on 15-09-2021.
+//
+
