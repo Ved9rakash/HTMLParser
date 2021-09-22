4e71c8dc62dfd1c46f47f1877f091e308ce2a702
Vaibhav Sharma <vs19990711@gmail.com>
2021-09-22 20:07:18 +0530

    Parser

--- src/HTMLParser.cpp
+++ src/HTMLParser.cpp
@@ -1,4 +1,5 @@
 #include "../include/PageGrabber.hpp"
+//#include "../include/Parse.hpp"
 
 #include <iostream>
 #include <curl/curl.h>
@@ -7,31 +8,39 @@
 
 int main()
 {
+    const std::string fileName = "Download.txt";
+    //Creating handles to download network content.
     std::vector<EasyHandle> handles(1);
-    try
-    {
-        std::for_each(
-                handles.begin(), handles.end(), [] (auto& handle){ handle = sync::CreateEasyHandle(); }
-                );
+
+
+    //Create easy handles and Loop through the EasyHandle container.
+    try{
+        std::for_each(handles.begin(), handles.end(), [] (auto& handle){ handle = sync::CreateEasyHandle(); });
     }
-    catch(const std::exception& e)
-    {
+    catch(const std::exception& e){
         std::cerr << e.what() << '\n';
         return -1;
     }
 
     for (auto& handle : handles)
     {
+        //Setting the options and URL for handles which to be downloaded.
         curl_easy_setopt(handle.get(), CURLOPT_URL, "https://github.com/LesleyLai/cmake-cpp-boilerplate/blob/main/CMakeLists.txt");
+        //switching OFF the SSL connection (Less complex)
         sync::setSSL(handle.get());
-        sync::fileHandle::saveToFile(handle.get(), "Downloaded.txt");
+        //Saving the extracted handled data to file name 'fileName'.
+        sync::fileHandle::saveToFile(handle.get(), fileName);
 
-        auto res = curl_easy_perform(handle.get());
-        if (res != CURLE_OK)
-        {
-            std::cerr << "curl_easy_perform() failed." << curl_easy_strerror(res) << '\n';
+        //Perform the requests, requests with return code.
+        auto requests = curl_easy_perform(handle.get());
+        if (requests != CURLE_OK){
+            std::cerr << "curl_easy_perform() failed." << curl_easy_strerror(requests) << '\n';
             return -1;
         }
     }
+
+    //HTML Parsing
+//    Parse parseData;
+
     return 0;
 }
