5ccacfe33e93caea1a3ebc7520ca1a80ce0a370d
Vaibhav Sharma <vs19990711@gmail.com>
2021-09-22 22:45:39 +0530

    ParseLogic

--- src/Parse.cpp
+++ src/Parse.cpp
@@ -1,4 +1,45 @@
-//
-// Created by Vaibhav on 15-09-2021.
-//
 
+#include "../include/Parse.hpp"
+#include <string>
+#include <fstream>
+#include <algorithm>
+
+
+Parse::Parse() { }
+
+Parse::Parse(std::string fileName, std::string tagName)
+{
+    m_fileName = fileName;
+    m_tag = tagName;
+}
+
+
+void Parse::setFile(const std::string fileName)
+{
+    m_fileName = fileName;
+}
+
+void Parse::setTag(std::string tagName)
+{
+    m_tag = tagName;
+}
+
+void Parse::GetData()
+{
+    m_fileName =  "../cmake-build-debug/src/" + m_fileName;
+    std::pair<std::string, std::string> data;
+    std::ifstream file(m_fileName);
+    std::string savedFile = m_tag + ".txt";
+    std::string currentLine;
+    if (file.is_open())
+    {
+        while (std::getline(file, currentLine))
+        {
+
+            //Check if it exists
+            if (currentLine.substr(0, m_tag.length() + 1) == ("<" + m_tag)) {
+
+            }
+        }
+    }
+}
